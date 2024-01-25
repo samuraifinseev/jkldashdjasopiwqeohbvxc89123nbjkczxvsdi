5d4b7d30369015c25bdd22c0781be219f7e85ea9
samuraifinseev <155964563+samuraifinseev@users.noreply.github.com>
2024-01-25 14:20:12 +0500

    Add files via upload

---
+++ ip_send_from_rdp_to_bot.py
@@ -0,0 +1,42 @@
+import requests, time
+vms_info_file = open('test.csv')
+data_of_vms_info_file = vms_info_file.read()
+list_of_vms_info = data_of_vms_info_file.split('\n'); #list_of_vms_info.sort(); #del list_of_ips[0:3];
+ready_list_of_ips = []
+del list_of_vms_info[0]; del list_of_vms_info[len(list_of_vms_info) - 1]
+for i in range(len(list_of_vms_info)):
+    list_of_vms_info[i] = list_of_vms_info[i][list_of_vms_info[i].find('IPAddresses'):]
+list_of_vms_info.sort() # ОТСОРТИРОВАЛИ
+text_serv_group = 'server_group'
+def sort_ips_from_file(list_of_vms, start_vms, number_of_vms):
+    ips_str = ''
+    for i in range(start_vms, number_of_vms):
+        if text_serv_group in list_of_vms[i] or text_serv_group.upper() in list_of_vms[i]:
+            temp_stroka_ip = list_of_vms_info[i][list_of_vms[i].find('PublicIP') + 15:]
+            temp_stroka_ip = temp_stroka_ip[:temp_stroka_ip.find('"')]
+            ready_list_of_ips.append(temp_stroka_ip)
+            ips_str += temp_stroka_ip + '\n'
+    return ips_str
+    #return ready_list_of_ips
+def from_rdp_to_tg(ips):
+    TOKEN = "6619003611:AAGRivvPR1q5XZbnNh0RgZ5Y86_FBlpkTOE"
+    chat_id = '506640934'
+    message = ips
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+    print(requests.get(url).json())  # Эта строка отсылает сообщение
+if len(list_of_vms_info) == 3:
+    ips_serv_group = sort_ips_from_file(list_of_vms_info, 0, 3)
+    from_rdp_to_tg(ips_serv_group)
+    print(ips_serv_group)
+if len(list_of_vms_info) == 8:
+    ips_serv_group = sort_ips_from_file(list_of_vms_info, 3, 8)
+    from_rdp_to_tg(ips_serv_group)
+    print(ips_serv_group)
+if len(list_of_vms_info) == 14:
+    ips_serv_group = sort_ips_from_file(list_of_vms_info, 8, 14)
+    from_rdp_to_tg(ips_serv_group)
+    print(ips_serv_group)
+if len(list_of_vms_info) == 17:
+    ips_serv_group = sort_ips_from_file(list_of_vms_info, 14, 17)
+    from_rdp_to_tg(ips_serv_group)
+    print(ips_serv_group)
