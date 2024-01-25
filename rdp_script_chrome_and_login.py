a6397d91d00c549f797277f63360a35aae7daf82
samuraifinseev <155964563+samuraifinseev@users.noreply.github.com>
2024-01-25 10:00:06 +0500

    Update rdp_script_chrome_and_login.py

--- rdp_script_chrome_and_login.py
+++ rdp_script_chrome_and_login.py
@@ -105,8 +105,9 @@ click(102, 489, 1, 0, 'left'); keyboard.write('56981488228Simak', delay=0.1); sl
 click(110, 629, 1, 0, 'left'); sleep(18) # подтверждаем изменение пасса
 press('f5'); sleep(20) # refresh page
 #click(1020, 366, 1, 0, 'left'); sleep(4) # отменить сохранение данных лог пасса
-click(490, 485, 1, 0, 'left');
-click(446, 482, 1, 0, 'left'); keyboard.write('56981488228Simak', delay=0.2); click(717, 645, 1, 0, 'left'); sleep(15) # перелогин на случай вылета из панели
+#click(490, 485, 1, 0, 'left');
+#click(446, 482, 1, 0, 'left');
+keyboard.write('56981488228Simak', delay=0.2); click(717, 645, 1, 0, 'left'); click(717, 655, 1, 0, 'left'); click(717, 635, 1, 0, 'left'); sleep(15) # перелогин на случай вылета из панели
 click(1010, 362, 1, 0, 'left'); click(806, 142, 1, 0, 'left'); sleep(11) # закрываем еще одно окно навязывания хуйни для защиты
 click(717, 621, 1, 0, 'left'); sleep(20); # переходим в секьюрити
 scroll(-3200); sleep(2); click(123, 725, 1, 0, 'left'); sleep(7); click(491, 551, 1, 0, 'left'); sleep(2) # скролим вниз и генерим новый код и подтверждаем
