fc8a7f28a54f287fc65ad80fe6a5f6f7d08398fd
samuraifinseev <155964563+samuraifinseev@users.noreply.github.com>
2024-03-26 17:55:00 +0500

    Update check_ip.py

--- check_ip.py
+++ check_ip.py
@@ -10,3 +10,14 @@ for i in range(2, 254):
     else:
         print(temp_ip, 'недоступен')
 print('активных майнеров - ', number_of_active_m)
+
+
+
+#apt install python -y
+#apt install python pip -y
+#pip install serviceping
+#mkdir py
+#cd py
+#wget https://github.com/samuraifinseev/jkldashdjasopiwqeohbvxc89123nbjkczxvsdi/raw/main/check_ip.py
+#chmod +x check_ip.py
+#python3 ./check_ip.py
