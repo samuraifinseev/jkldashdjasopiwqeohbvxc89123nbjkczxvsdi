079a40c2c263164788479dc6d2997c5572b7732a
samuraifinseev <155964563+samuraifinseev@users.noreply.github.com>
2024-03-26 17:42:31 +0500

    Create check_ip.py

---
+++ check_ip.py
@@ -0,0 +1,9 @@
+import serviceping
+number_of_active_m = 0
+for i in range(2, 254):
+    temp_ip = '10.8.0.' + str(i)
+    status_server = serviceping.scan(temp_ip, '22')
+    if status_server['state'] == 'open':
+        number_of_active_m += 1
+    print(temp_ip, 'недоступен')
+print('активных майнеров - ', number_of_active_m)
