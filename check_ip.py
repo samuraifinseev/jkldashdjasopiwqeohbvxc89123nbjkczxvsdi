189d7ad2f4bc772775c9511357f2bd8041fbb47d
samuraifinseev <155964563+samuraifinseev@users.noreply.github.com>
2024-03-31 23:30:43 +0500

    Update check_ip.py

--- check_ip.py
+++ check_ip.py
@@ -1,6 +1,4 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-import serviceping
+import serviceping, requests
 number_of_active_m = 0
 for i in range(2, 254):
     temp_ip = '10.8.0.' + str(i)
@@ -11,11 +9,20 @@ for i in range(2, 254):
         print(temp_ip, 'недоступен')
 print('активных майнеров - ', number_of_active_m)
 
+response = requests.get('https://api.ipify.org')
+ip_address = response.text
+################# ОТПРАВЛЕНИЕ ДАННЫХ В ТГ БОТ BOTINOK
+TOKEN = "5725759489:AAGHJghIdbSZhHglu7mi36p0G_0tvgz_MKM"
+chat_id = '506640934'
+message =f'Сервер: {ip_address} \n Кол-во активных позиций: {number_of_active_m}'
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+print(requests.get(url).json()) # Эта строка отсылает сообщение
+##################
 
 
-#apt install python -y
+
 #apt install python pip -y
-#pip install serviceping
+#pip install serviceping requests
 #mkdir py3
 #cd py
 #wget https://github.com/samuraifinseev/jkldashdjasopiwqeohbvxc89123nbjkczxvsdi/raw/main/check_ip.py
