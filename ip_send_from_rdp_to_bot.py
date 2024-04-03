7a62a3e8fc24f65346fcd87c6d9ae13fbede8ff3
samuraifinseev <155964563+samuraifinseev@users.noreply.github.com>
2024-04-03 16:40:05 +0500

    Update ip_send_from_rdp_to_bot.py

--- ip_send_from_rdp_to_bot.py
+++ ip_send_from_rdp_to_bot.py
@@ -36,12 +36,12 @@ try:
         ips_serv_group = sort_ips_from_file(list_of_vms_info, 0, 3)
         from_rdp_to_tg(ips_serv_group, 1)
         print(ips_serv_group)
-    if len(list_of_vms_info) == 8:
-        ips_serv_group = sort_ips_from_file(list_of_vms_info, 3, 8)
+    if len(list_of_vms_info) == 7:
+        ips_serv_group = sort_ips_from_file(list_of_vms_info, 3, 7)
         from_rdp_to_tg(ips_serv_group, 2)
         print(ips_serv_group)
-    if len(list_of_vms_info) == 13:
-        ips_serv_group = sort_ips_from_file(list_of_vms_info, 8, 13)
+    if len(list_of_vms_info) == 12:
+        ips_serv_group = sort_ips_from_file(list_of_vms_info, 8, 12)
         from_rdp_to_tg(ips_serv_group, 3)
         print(ips_serv_group)
     if len(list_of_vms_info) == 17:
