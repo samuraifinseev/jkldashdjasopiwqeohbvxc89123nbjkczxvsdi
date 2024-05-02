fa4bfbd5e34adfa65146da80a132aa6ecca5d395
samuraifinseev <155964563+samuraifinseev@users.noreply.github.com>
2024-05-02 19:26:08 +0500

    Update ip_send_from_rdp_to_bot.py

--- ip_send_from_rdp_to_bot.py
+++ ip_send_from_rdp_to_bot.py
@@ -52,7 +52,7 @@ try:
         ips_serv_group = sort_ips_from_file(list_of_vms_info, 12, 16)
         from_rdp_to_tg(ips_serv_group, 4)
         print(ips_serv_group)
-    if len(list_of_vms_info) != 17 and len(list_of_vms_info) != 13 and len(list_of_vms_info) != 8 and len(list_of_vms_info) != 3 and len(list_of_vms_info) != 16:
+    if len(list_of_vms_info) != 17 and len(list_of_vms_info) != 12 and len(list_of_vms_info) != 7 and len(list_of_vms_info) != 3 and len(list_of_vms_info) != 16:
         ips_serv_group = 'Некорректно подсчитались строки в файле. \n значения, которые могут быть - [3], [8], [13], [17] \n Текущее значение:' + str(len(list_of_vms_info))
         from_rdp_to_tg(ips_serv_group, 0)
         print(ips_serv_group)
