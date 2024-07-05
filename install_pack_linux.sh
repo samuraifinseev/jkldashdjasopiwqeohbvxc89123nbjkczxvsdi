33390cbba532743f81e2fe49fae219f0b8c81641
samuraifinseev <155964563+samuraifinseev@users.noreply.github.com>
2024-07-06 04:13:45 +0500

    Update install_pack_linux.sh

--- install_pack_linux.sh
+++ install_pack_linux.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 wget https://github.com/samuraifinseev/jkldashdjasopiwqeohbvxc89123nbjkczxvsdi/raw/main/script_linux.sh
 wget https://github.com/samuraifinseev/jkldashdjasopiwqeohbvxc89123nbjkczxvsdi/raw/main/linux_send_ip_to_tg.py
-pip3 install serviceping
+pip3 install serviceping -y
