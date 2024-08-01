9a7504eb4c4fc0cfbd2d9b9a2641ec92c58e8461
samuraifinseev <155964563+samuraifinseev@users.noreply.github.com>
2024-08-01 18:05:30 +0500

    Create temp_scr.sh

---
+++ temp_scr.sh
@@ -0,0 +1,21 @@
+# !/bin/bash
+cd /home/azureuser
+az vm restart -g servers_group -n server10
+az vm restart -g servers_group -n server11
+az vm restart -g servers_group -n server12
+az vm restart -g servers_group -n server14
+az vm restart -g servers_group -n server15
+az vm restart -g servers_group -n server16
+az vm restart -g servers_group -n server17
+az vm restart -g servers_group -n server18
+az vm restart -g servers_group -n server19
+az vm restart -g servers_group -n server20
+az vm restart -g servers_group -n server21
+az vm restart -g servers_group -n server22
+az vm restart -g servers_group -n server23
+az vm restart -g servers_group -n server24
+az vm restart -g servers_group -n server27
+az vm restart -g servers_group -n server28
+az vm restart -g servers_group -n server29
+sleep 30
+python3 linux_send_ip_to_tg.py
